191fe9805c85e61326520867777723814f91bad1
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-07-20 16:44:07 +0200

    Update scalafmt-core to 3.11.4 (#79)

--- it/shared/src/test/scala/pgmq4s/it/PgmqTestConfig.scala
+++ it/shared/src/test/scala/pgmq4s/it/PgmqTestConfig.scala
@@ -1,11 +1,11 @@
 package pgmq4s.it
 
 case class PgmqTestConfig(
-  host: String = "localhost",
-  port: Int = 5433,
-  database: String = "pgmq",
-  user: String = "pgmq",
-  password: String = "pgmq"
+    host: String = "localhost",
+    port: Int = 5433,
+    database: String = "pgmq",
+    user: String = "pgmq",
+    password: String = "pgmq"
 ):
   def jdbcUrl: String = s"jdbc:postgresql://$host:$port/$database"
 
